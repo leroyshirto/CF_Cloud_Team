1b1c1450013c864dddc5c6e2bbf67e87a07481e4
Simon <simonpelczer@outlook.de>
2019-03-24 14:58:47 +0100

    :recycle: Removed GS infavor of S3

--- Prediction/predict.py
+++ Prediction/predict.py
@@ -2,29 +2,36 @@ from openvino.inference_engine import IENetwork, IEPlugin
 import argparse
 import numpy as np
 from urllib.parse import urlparse
-from google.cloud import storage
-from google.auth import exceptions
 import classes
 import datetime
 from shutil import copy
 import os
 import json
+from minio import Minio
+from minio.error import ResponseError
+
+minioClient = Minio('212.227.4.254:9000',
+                    access_key='AKIAIOSFODNN7EXAMPLE',
+                    secret_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+                    secure=False)
+
+''' source_path = "s3://bucke_name/filename[file_type] '''
+
 
 def get_local_file(source_path):
     parsed_path = urlparse(source_path)
-    if parsed_path.scheme == "gs":
+    if parsed_path.scheme == "s3":
         bucket_name = parsed_path.netloc
-        file_path = parsed_path.path[1:]
-        file_name = os.path.split(parsed_path.path)[1]
+        file_name = parsed_path.path[1:]
+        print(f"Extracted Bucket Name = {bucket_name} File Name = {file_name}")
+
         try:
-            gs_client = storage.Client()
-            bucket = gs_client.get_bucket(bucket_name)
-        except exceptions.DefaultCredentialsError:
-            # if credentials fails, try to connect as anonymous user
-            gs_client = storage.Client.create_anonymous_client()
-            bucket = gs_client.bucket(bucket_name, user_project=None)
-        blob = bucket.blob(file_path)
-        blob.download_to_filename(file_name)
+            data = minioClient.get_object(bucket_name, file_name)
+            with open(file_name, 'wb') as file_data:
+                for d in data.stream(32*1024):
+                    file_data.write(d)
+        except ResponseError as err:
+            print(err)
     elif parsed_path.scheme == "":
         # in case of local path just pass the input argument
         if os.path.isfile(source_path):
@@ -35,22 +42,25 @@ def get_local_file(source_path):
     return file_name
 
 
-def upload_file(source_file, target_folder):
-    parsed_path = urlparse(target_folder)
-    if parsed_path.scheme == "gs":
-        bucket_name = parsed_path.netloc
-        folder_path = parsed_path.path[1:]
+''' output_path = "s3://bucke_name/filename[file_type] '''
+
+
+def upload_file(output_bucket, file_name):
+    parsed_path = urlparse(output_bucket)
+    if parsed_path.scheme == "s3":
+        output_bucket = parsed_path.netloc
+        print(f"Using Bucket Name = {output_bucket} File Name = {file_name}")
+
         try:
-            gs_client = storage.Client()
-            bucket = gs_client.get_bucket(bucket_name)
-            blob = bucket.blob(folder_path + "/" + source_file)
-            blob.upload_from_filename(source_file)
-        except Exception as er:
-            print(er)
-            return False
+            with open(file_name, 'rb') as file_data:
+                file_stat = os.stat(file_name)
+                minioClient.put_object(output_bucket, file_name,
+                                       file_data, file_stat.st_size)
+        except ResponseError as err:
+            print(err)
     elif parsed_path.scheme == "":
-        if target_folder != ".":
-            copy(source_file, target_folder)
+        if output_bucket != ".":
+            copy(file_name, output_bucket)
     return True
 
 
@@ -61,12 +71,12 @@ def main():
                         help='GCS or local path to model weights file (.bin)')
     parser.add_argument('--model_xml', type=str, required=True,
                         help='GCS or local path to model graph (.xml)')
-    parser.add_argument('--input_numpy_file', type=str, required=True,
+    parser.add_argument('--input_numpy_file', type=str, required=True,  # Provided by Fetch Script
                         help='GCS or local path to input dataset numpy file')
-    parser.add_argument('--label_numpy_file', type=str, required=True,
+    parser.add_argument('--label_numpy_file', type=str, required=True,  # Provided by Fetch Script
                         help='GCS or local path to numpy file with labels')
-    parser.add_argument('--output_folder', type=str, required=True,
-                        help='GCS or local path to results upload folder')
+    parser.add_argument('--output_bucket', type=str, required=True,
+                        help='S3 Compatible Bucket or local path to results upload folder')
     parser.add_argument('--batch_size', type=int, default=1,
                         help='batch size to be used for inference')
     parser.add_argument('--scale_div', type=float, default=1,
@@ -124,10 +134,11 @@ def main():
     imgs = (imgs / args.scale_div) - args.scale_div
     lbs = np.load(label_numpy_file, mmap_mode='r', allow_pickle=False)
 
-    print("Loaded input data", imgs.shape, imgs.dtype, "Min value:", np.min(imgs), "Max value", np.max(imgs))
+    print("Loaded input data", imgs.shape, imgs.dtype,
+          "Min value:", np.min(imgs), "Max value", np.max(imgs))
 
     combined_results = {}  # dictionary storing results for all model outputs
-    processing_times = np.zeros((0),int)
+    processing_times = np.zeros((0), int)
     matched_count = 0
     total_executed = 0
 
@@ -139,19 +150,21 @@ def main():
         end_time = datetime.datetime.now()
         duration = (end_time - start_time).total_seconds() * 1000
         print("Inference duration:", duration, "ms")
-        processing_times = np.append(processing_times,np.array([int(duration)]))
-        output = list(results.keys())[0] # check only one output
+        processing_times = np.append(
+            processing_times, np.array([int(duration)]))
+        output = list(results.keys())[0]  # check only one output
         nu = results[output]
         for i in range(nu.shape[0]):
-            single_result = nu[[i],...]
+            single_result = nu[[i], ...]
             ma = np.argmax(single_result)
             total_executed += 1
             if ma == lb[i]:
                 matched_count += 1
                 mark_message = "; Correct match."
             else:
-                mark_message = "; Incorrect match. Should be {} {}".format(lb[i], classes.imagenet_classes[lb[i]] )
-            print("\t",i, classes.imagenet_classes[ma],ma, mark_message)
+                mark_message = "; Incorrect match. Should be {} {}".format(
+                    lb[i], classes.imagenet_classes[lb[i]])
+            print("\t", i, classes.imagenet_classes[ma], ma, mark_message)
         if output in combined_results:
             combined_results[output] = np.append(combined_results[output],
                                                  results[output], 0)
@@ -162,16 +175,19 @@ def main():
     np.save(filename, combined_results[output])
     upload_file(filename, args.output_folder)
     print("Inference results uploaded to", filename)
-    print('Classification accuracy: {:.2f}'.format(100*matched_count/total_executed))
-    print('Average time: {:.2f} ms; average speed: {:.2f} fps'.format(round(np.average(processing_times), 2),round(1000 * batch_size / np.average(processing_times), 2)))
+    print('Classification accuracy: {:.2f}'.format(
+        100*matched_count/total_executed))
+    print('Average time: {:.2f} ms; average speed: {:.2f} fps'.format(round(np.average(
+        processing_times), 2), round(1000 * batch_size / np.average(processing_times), 2)))
 
     accuracy = matched_count/total_executed
     latency = np.average(processing_times)
-    metrics = {'metrics': [{'name': 'accuracy-score','numberValue':  accuracy,'format': "PERCENTAGE"},
-                           {'name': 'latency','numberValue':  latency,'format': "RAW"}]}
+    metrics = {'metrics': [{'name': 'accuracy-score', 'numberValue':  accuracy, 'format': "PERCENTAGE"},
+                           {'name': 'latency', 'numberValue':  latency, 'format': "RAW"}]}
 
     with open('/mlpipeline-metrics.json', 'w') as f:
         json.dump(metrics, f)
+
 
 if __name__ == "__main__":
     main()
