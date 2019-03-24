8c5eff2ca8013a63c143e6c833c467bccba1e92f
Simon <simonpelczer@outlook.de>
2019-03-24 18:14:31 +0100

    :recycle: Updated Pipeline

--- Pipeline/prediction_pipeline.py
+++ Pipeline/prediction_pipeline.py
@@ -5,11 +5,11 @@ import kfp.dsl as dsl
     name='Prediction pipeline',
     description='Execute prediction operation for the dataset from numpy file and test accuracy and latency'
 )
-def openvino_predict(
+def emotion_pipeline(
         model_bin=dsl.PipelineParam(
-            name='model-bin-path', value='s3://models/resnet_50_i8.bin'),
+            name='model-bin-path', value='s3://models/emotions_f32.bin'),
         model_xml=dsl.PipelineParam(
-            name='model-xml-path', value='s3://models/resnet_50_i8.xml'),
+            name='model-xml-path', value='s3://models/emotions_f32.xml'),
         generated_model_dir=dsl.PipelineParam(
             name='generated-model-dir', value='s3://output'),
         input_numpy_file=dsl.PipelineParam(
@@ -19,15 +19,23 @@ def openvino_predict(
         batch_size=dsl.PipelineParam(name='batch-size', value=1),
         scale_div=dsl.PipelineParam(name='scale-input-divide', value=1),
         scale_sub=dsl.PipelineParam(name='scale-input-substract', value=0)):
-    """A one-step pipeline."""
-    dsl.ContainerOp(
+    """A pipeline with two sequential steps."""
+
+    fetch_step = dsl.ContainerOp(
+        name='fetch',
+        image='library/bash:4.4.23',
+        command=['sh', '-c'],
+        arguments=['touch /tmp/test-output'],
+        file_outputs={'downloaded': '/tmp/test-output'})
+
+    build_step = dsl.ContainerOp(
         name='calc-engine',
         image='templum/openvino-serve:2',
         command=['python3', 'predict.py'],
         arguments=[
             '--model_bin', model_bin,
             '--model_xml', model_xml,
-            '--input_numpy_file', input_numpy_file,
+            '--input_numpy_file', fetch_step.output,
             '--label_numpy_file', label_numpy_file,
             '--batch_size', batch_size,
             '--scale_div', scale_div,
@@ -38,4 +46,4 @@ def openvino_predict(
 
 if __name__ == '__main__':
     import kfp.compiler as compiler
-    compiler.Compiler().compile(openvino_predict, __file__ + '.tar.gz')
+    compiler.Compiler().compile(emotion_pipeline, __file__ + '.tar.gz')
