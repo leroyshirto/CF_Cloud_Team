20ab8c60ef81f887a302dde13d038d16761c8ff3
Simon <simonpelczer@outlook.de>
2019-03-24 16:27:00 +0100

    :sparkles: Corrected default values

--- Pipeline/prediction_pipeline.py
+++ Pipeline/prediction_pipeline.py
@@ -7,15 +7,15 @@ import kfp.dsl as dsl
 )
 def openvino_predict(
         model_bin=dsl.PipelineParam(
-            name='model-bin-path', value='s3://212.227.4.254:9000/models/resnet_50_i8.bin'),
+            name='model-bin-path', value='s3://models/resnet_50_i8.bin'),
         model_xml=dsl.PipelineParam(
-            name='model-xml-path', value='s3://212.227.4.254:9000/models/resnet_50_i8.xml'),
+            name='model-xml-path', value='s3://models/resnet_50_i8.xml'),
         generated_model_dir=dsl.PipelineParam(
-            name='generated-model-dir', value='s3://212.227.4.254:9000/output'),
+            name='generated-model-dir', value='s3://output'),
         input_numpy_file=dsl.PipelineParam(
-            name='input-numpy-file', value='s3://212.227.4.254:9000/generated-models/imgs.npy'),
+            name='input-numpy-file', value='s3://generated-models/imgs.npy'),
         label_numpy_file=dsl.PipelineParam(
-            name='label-numpy-file', value='s3://212.227.4.254:9000/generated-models/lbs.npy'),
+            name='label-numpy-file', value='s3://generated-models/lbs.npy'),
         batch_size=dsl.PipelineParam(name='batch-size', value=1),
         scale_div=dsl.PipelineParam(name='scale-input-divide', value=1),
         scale_sub=dsl.PipelineParam(name='scale-input-substract', value=0)):
